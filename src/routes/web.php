c32bc61938c3984253d87bcb719c4c6bca755394
tangrowth <to.chinatsu@icloud.com>
2023-04-01 17:41:19 +0900

    「0-6 ブラウザに画面を表示しよう」まで完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestController::class, 'index']);
